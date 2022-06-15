56e887764deb1af82223b4495b716ca46e377349
ngactive99 <ngactive99@gmail.com>
2022-06-15 12:55:26 +0530

    intregrated nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserformComponent } from './components/userform/userform.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserformComponent
+    UserformComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
