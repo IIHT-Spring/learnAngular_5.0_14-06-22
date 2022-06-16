329cb01100f9ff97ff43a22c9fbc654c242fbef4
ngactive99 <ngactive99@gmail.com>
2022-06-16 09:57:39 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { UserformComponent } from './components/userform/userform.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'about', component: AboutComponent}, {path:'register', component:UserformComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
